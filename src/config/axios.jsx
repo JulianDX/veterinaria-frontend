0015154d514ffa353f33b4483d53170d21401209
Julian <jfroap@uqvirtual.edu.co>
2024-03-21 19:24:38 -0500

    modificacion axios

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosCliente = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
+  baseURL: `/api/${import.meta.env.VITE_BACKEND_URL}`,
 });
 
 export default axiosCliente;
